cebc9f337f7c44f22bcc6a65c5eeb6d61a96835d
Tung205 <158316550+Tung205@users.noreply.github.com>
2024-03-13 09:42:52 +0700

    Lec4_PhanA_Ex1
    Khởi tạo mảng

---
+++ Lec4/Lec4_PhanA_Ex1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+//int a[];
+int b[5] = {1,2,3,4};
+//int c[3] = {1,2,3,4};
+int d[] = {1,2,3,4};
+int main(){
+	//int A[];
+	int B[5] = {1,2,3,4};
+	//int C[3] = {1,2,3,4};
+	int D[] = {1,2,3,4};
+	/*for (int i = 0; i < 4; i++){
+		cout << a[i] << " ";
+	}
+	cout << endl;*/
+	for (int i = 0; i < 5; i++){
+		cout << b[i] << " ";
+	}
+	cout << endl;
+	/*for (int i = 0; i < 3; i++){
+		cout << c[i] << " ";
+	}
+	cout << endl;*/
+	for (int i = 0; i < 4; i++){
+		cout << d[i] << " ";
+	}
+	cout << endl;
+	/*for (int i = 0; i < 4; i++){
+		cout << A[i] << " ";
+	}
+	cout << endl;*/
+	for (int i = 0; i < 5; i++){
+		cout << B[i] << " ";
+	}
+	cout << endl;
+	/*for (int i = 0; i < 3; i++){
+		cout << C[i] << " ";
+	}
+	cout << endl;*/
+	for (int i = 0; i < 4; i++){
+		cout << D[i] << " ";
+	}
+	cout << endl;
+	
+}
